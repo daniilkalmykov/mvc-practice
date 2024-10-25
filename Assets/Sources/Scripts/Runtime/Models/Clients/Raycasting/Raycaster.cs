d222bc7c49e1bbf45213fa3a465c46705fbabff7
depressedbiba <daniil.kalmykov@gtapstudio.com>
2024-10-25 23:42:35 +0500

    add: CanBeNull annotation and return null in RaycastByComponent
    Introduce the CanBeNull annotation to the RaycastByComponent method to indicate that the return value can be null. Adjust return statements to explicitly return null instead of default for clearer intent and maintainability.

--- Assets/Sources/Scripts/Runtime/Models/Clients/Raycasting/Raycaster.cs
+++ Assets/Sources/Scripts/Runtime/Models/Clients/Raycasting/Raycaster.cs
@@ -1,15 +1,17 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Sources.Scripts.Runtime.Models.Clients.Raycasting
 {
-    public static class Raycaster<T> where T : MonoBehaviour
+    public static class Raycaster<T>  where T : MonoBehaviour
     {
+        [CanBeNull]
         public static T RaycastByComponent(Vector3 origin, Vector3 target)
         {
             if (Physics.Raycast(origin, target, out var hit) == false)
-                return default;
+                return null;
 
-            return hit.transform.TryGetComponent(out T t) ? t : default;
+            return hit.transform.TryGetComponent(out T t) ? t : null;
         }
     }
 }
